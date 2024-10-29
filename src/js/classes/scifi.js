df92b087d8e256102a877b03580327fb92fc836c
Raul GM <loscanos3raul@gmail.com>
2024-10-29 19:44:15 +0100

    finalizado

--- src/js/classes/scifi.js
+++ src/js/classes/scifi.js
@@ -1,201 +1,201 @@
 export const films = [
     {
-        title: "Galactic Frontier",
-        original_title: "Frontera Galáctica",
-        description: "A group of astronauts explores the outer edges of the galaxy, encountering mysterious alien civilizations.",
-        director: "Lina Corso",
-        release_date: "2035-04-22",
-        cover_image: "./../../assets/galacticFrontier.jpg",
-        cast: [
+        "title": "Galactic Frontier",
+        "original_title": "Frontera Galáctica",
+        "description": "A group of astronauts explores the outer edges of the galaxy, encountering mysterious alien civilizations.",
+        "director": "Lina Corso",
+        "release_date": "2035-04-22",
+        "cover_image": "./src/assets/galacticFrontier.jpg",
+        "cast": [
             {
-                name: "Captain Nova",
-                gender: "Female",
-                birth_date: "1990-07-15"
+                "name": "Captain Nova",
+                "gender": "Female",
+                "birth_date": "1990-07-15"
             },
             {
-                name: "Dr. Orion",
-                gender: "Male",
-                birth_date: "1982-11-02"
+                "name": "Dr. Orion",
+                "gender": "Male",
+                "birth_date": "1982-11-02"
             }
         ]
     },
     {
-        title: "Chrono Shift",
-        original_title: "Cambio Temporal",
-        description: "Scientists discover a way to manipulate time, but their experiment goes terribly wrong, leading to catastrophic consequences.",
-        director: "Emilio Drayton",
-        release_date: "2041-11-11",
-        cover_image: "./../../assets/chronoShift.jpg",
-        cast: [
+        "title": "Chrono Shift",
+        "original_title": "Cambio Temporal",
+        "description": "Scientists discover a way to manipulate time, but their experiment goes terribly wrong, leading to catastrophic consequences.",
+        "director": "Emilio Drayton",
+        "release_date": "2041-11-11",
+        "cover_image": "./src/assets/chronoShift.jpg",
+        "cast": [
             {
-                name: "Dr. Amelia Sparks",
-                gender: "Female",
-                birth_date: "1985-04-23"
+                "name": "Dr. Amelia Sparks",
+                "gender": "Female",
+                "birth_date": "1985-04-23"
             },
             {
-                name: "Agent Felix",
-                gender: "Male",
-                birth_date: "1993-09-10"
+                "name": "Agent Felix",
+                "gender": "Male",
+                "birth_date": "1993-09-10"
             }
         ]
     },
     {
-        title: "Echoes of Mars",
-        original_title: "Ecos de Marte",
-        description: "A colony on Mars is haunted by strange echoes from the planet's past, as settlers uncover an ancient Martian civilization.",
-        director: "Zara Linton",
-        release_date: "2029-07-29",
-        cover_image: "./../../assets/echosOfMars.jpg",
-        cast: [
+        "title": "Echoes of Mars",
+        "original_title": "Ecos de Marte",
+        "description": "A colony on Mars is haunted by strange echoes from the planet's past, as settlers uncover an ancient Martian civilization.",
+        "director": "Zara Linton",
+        "release_date": "2029-07-29",
+        "cover_image": "./src/assets/echosOfMars.jpg",
+        "cast": [
             {
-                name: "Commander Drake",
-                gender: "Male",
-                birth_date: "1978-03-11"
+                "name": "Commander Drake",
+                "gender": "Male",
+                "birth_date": "1978-03-11"
             },
             {
-                name: "Lt. Naomi Vega",
-                gender: "Female",
-                birth_date: "1992-06-30"
+                "name": "Lt. Naomi Vega",
+                "gender": "Female",
+                "birth_date": "1992-06-30"
             }
         ]
     },
     {
-        title: "The Matrix",
-        original_title: "The Matrix",
-        description: "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
-        director: "The Wachowskis",
-        release_date: "1999-03-31",
-        cover_image: "./../../assets/Matrix.jpg",
-        cast: [
+        "title": "The Matrix",
+        "original_title": "The Matrix",
+        "description": "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+        "director": "The Wachowskis",
+        "release_date": "1999-03-31",
+        "cover_image": "./src/assets/Matrix.jpg",
+        "cast": [
             {
-                name: "Neo",
-                gender: "Male",
-                birth_date: "1974-09-02"
+                "name": "Neo",
+                "gender": "Male",
+                "birth_date": "1974-09-02"
             },
             {
-                name: "Trinity",
-                gender: "Female",
-                birth_date: "1968-03-15"
+                "name": "Trinity",
+                "gender": "Female",
+                "birth_date": "1968-03-15"
             }
         ]
     },
     {
-        title: "Blade Runner",
-        original_title: "Blade Runner",
-        description: "A blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
-        director: "Ridley Scott",
-        release_date: "1982-06-25",
-        cover_image: "./../../assets/BladeRunner.jpg",
-        cast: [
+        "title": "Blade Runner",
+        "original_title": "Blade Runner",
+        "description": "A blade runner must pursue and terminate four replicants who stole a ship in space and have returned to Earth to find their creator.",
+        "director": "Ridley Scott",
+        "release_date": "1982-06-25",
+        "cover_image": "./src/assets/BladeRunner.jpg",
+        "cast": [
             {
-                name: "Rick Deckard",
-                gender: "Male",
-                birth_date: "1948-01-13"
+                "name": "Rick Deckard",
+                "gender": "Male",
+                "birth_date": "1948-01-13"
             },
             {
-                name: "Rachael",
-                gender: "Female",
-                birth_date: "1961-08-11"
+                "name": "Rachael",
+                "gender": "Female",
+                "birth_date": "1961-08-11"
             }
         ]
     },
     {
-        title: "Inception",
-        original_title: "Inception",
-        description: "A thief who enters the dreams of others is tasked with planting an idea into the mind of a powerful individual.",
-        director: "Christopher Nolan",
-        release_date: "2010-07-16",
-        cover_image: "./../../assets/inception.jpg",
-        cast: [
+        "title": "Inception",
+        "original_title": "Inception",
+        "description": "A thief who enters the dreams of others is tasked with planting an idea into the mind of a powerful individual.",
+        "director": "Christopher Nolan",
+        "release_date": "2010-07-16",
+        "cover_image": "./src/assets/inception.jpg",
+        "cast": [
             {
-                name: "Dom Cobb",
-                gender: "Male",
-                birth_date: "1975-06-08"
+                "name": "Dom Cobb",
+                "gender": "Male",
+                "birth_date": "1975-06-08"
             },
             {
-                name: "Mal Cobb",
-                gender: "Female",
-                birth_date: "1978-09-21"
+                "name": "Mal Cobb",
+                "gender": "Female",
+                "birth_date": "1978-09-21"
             }
         ]
     },
     {
-        title: "2001: A Space Odyssey",
-        original_title: "2001: A Space Odyssey",
-        description: "A space voyage to Jupiter with a sentient computer leads to a terrifying confrontation between man and machine.",
-        director: "Stanley Kubrick",
-        release_date: "1968-04-02",
-        cover_image: "./../../assets/aSpaceOdyssey.png",
-        cast: [
+        "title": "2001: A Space Odyssey",
+        "original_title": "2001: A Space Odyssey",
+        "description": "A space voyage to Jupiter with a sentient computer leads to a terrifying confrontation between man and machine.",
+        "director": "Stanley Kubrick",
+        "release_date": "1968-04-02",
+        "cover_image": "./src/assets/aSpaceOdyssey.png",
+        "cast": [
             {
-                name: "Dr. Dave Bowman",
-                gender: "Male",
-                birth_date: "1934-06-19"
+                "name": "Dr. Dave Bowman",
+                "gender": "Male",
+                "birth_date": "1934-06-19"
             },
             {
-                name: "HAL 9000",
-                gender: "N/A",
-                birth_date: "1992-01-12"
+                "name": "HAL 9000",
+                "gender": "N/A",
+                "birth_date": "1992-01-12"
             }
         ]
     },
     {
-        title: "Star Wars: A New Hope",
-        original_title: "Star Wars",
-        description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy from the Empire.",
-        director: "George Lucas",
-        release_date: "1977-05-25",
-        cover_image: "./../../assets/starWarsANewHope.jpg",
-        cast: [
+        "title": "Star Wars: A New Hope",
+        "original_title": "Star Wars",
+        "description": "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee, and two droids to save the galaxy from the Empire.",
+        "director": "George Lucas",
+        "release_date": "1977-05-25",
+        "cover_image": "./src/assets/starWarsANewHope.jpg",
+        "cast": [
             {
-                name: "Luke Skywalker",
-                gender: "Male",
-                birth_date: "1951-09-25"
+                "name": "Luke Skywalker",
+                "gender": "Male",
+                "birth_date": "1951-09-25"
             },
             {
-                name: "Princess Leia",
-                gender: "Female",
-                birth_date: "1956-10-21"
+                "name": "Princess Leia",
+                "gender": "Female",
+                "birth_date": "1956-10-21"
             }
         ]
     },
     {
-        title: "Alien",
-        original_title: "Alien",
-        description: "The crew of a space tug discover a deadly alien organism on a distant planet, and must fight for survival.",
-        director: "Ridley Scott",
-        release_date: "1979-05-25",
-        cover_image: "./../../assets/Alien.webp",
-        cast: [
+        "title": "Alien",
+        "original_title": "Alien",
+        "description": "The crew of a space tug discover a deadly alien organism on a distant planet, and must fight for survival.",
+        "director": "Ridley Scott",
+        "release_date": "1979-05-25",
+        "cover_image": "./src/assets/Alien.webp",
+        "cast": [
             {
-                name: "Ellen Ripley",
-                gender: "Female",
-                birth_date: "1949-10-08"
+                "name": "Ellen Ripley",
+                "gender": "Female",
+                "birth_date": "1949-10-08"
             },
             {
-                name: "Ash",
-                gender: "Male",
-                birth_date: "1938-01-04"
+                "name": "Ash",
+                "gender": "Male",
+                "birth_date": "1938-01-04"
             }
         ]
     },
     {
-        title: "Interstellar",
-        original_title: "Interstellar",
-        description: "A group of explorers travel through a wormhole in space to ensure humanity's survival.",
-        director: "Christopher Nolan",
-        release_date: "2014-11-07",
-        cover_image: "./../../assets/interstellar.jpg",
-        cast: [
+        "title": "Interstellar",
+        "original_title": "Interstellar",
+        "description": "A group of explorers travel through a wormhole in space to ensure humanity's survival.",
+        "director": "Christopher Nolan",
+        "release_date": "2014-11-07",
+        "cover_image": "./src/assets/interstellar.jpg",
+        "cast": [
             {
-                name: "Cooper",
-                gender: "Male",
-                birth_date: "1969-10-21"
+                "name": "Cooper",
+                "gender": "Male",
+                "birth_date": "1969-10-21"
             },
             {
-                name: "Brand",
-                gender: "Female",
-                birth_date: "1984-02-11"
+                "name": "Brand",
+                "gender": "Female",
+                "birth_date": "1984-02-11"
             }
         ]
     }
